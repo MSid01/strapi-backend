3c927e0e25d37fa1a26dfbbd5affcbaa50213585
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-03-13 00:41:21 +0530

    serverjs changed

--- config/server.js
+++ config/server.js
@@ -1,4 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 3009),
+  url: env("MY_HEROKU_URL"),
+  proxy: true,
+  // app: {
+  //   keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  // },
 });
+
+//for localhost
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 3009),
+// });
