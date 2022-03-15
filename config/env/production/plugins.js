1efe6252bc385c1afe228c241300609a22d1e837
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-03-15 20:09:44 +0530

    playground disabled

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,7 +6,7 @@ module.exports = {
       config: {
         endpoint: '/graphql',
         shadowCRUD: true,
-        playgroundAlways: true,
+        playgroundAlways: false,
         depthLimit: 7,
         amountLimit: 100,
         apolloServer: {
