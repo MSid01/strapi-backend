d0265c3562746e7facae378b912432461226a607
Siddhesh <mungekarsiddhesh2001@gmail.com>
2022-03-15 17:52:57 +0530

    graphql playground plugin file added

--- src/api/rating/content-types/rating/lifecycles.js
+++ src/api/rating/content-types/rating/lifecycles.js
@@ -1,11 +1,4 @@
 module.exports = {
-  beforeCreate(event) {
-    const { data, where, select, populate } = event.params;
-    
-    // let's do a 20% discount everytime
-    // event.params.data.price = event.params.data.price * 0.8;
-  },
-
   async afterCreate(event) {
     const { result, params } = event;
     const garage_id = parseInt(params.data.garage);
